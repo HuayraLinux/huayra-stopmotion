d4f958f76385a2c047c0aa9a3608814f58ca0a0d
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-12-16 15:38:22 -0300

    Haciendo que la barra de controles para capturar cuadros se adapte al tamaño del contenedor.

--- src/js/app.js
+++ src/js/app.js
@@ -184,9 +184,11 @@ app.controller('AppCtrl', function ($scope) {
 	window.alternar_panel_lateral = function() {
 		var panel = document.getElementById('panel-lateral');
   	var contenedor = document.getElementById('contenedor-layers');
+  	var controles = document.getElementById('contenedor-controles');
 
 		panel.classList.toggle('panel-lateral-invisible');
 		contenedor.classList.toggle('contenedor-layers-expandido');
+		controles.classList.toggle('contenedor-controles-expandido');
 	}
 
   window.alternar_panel_ayuda = function() {
