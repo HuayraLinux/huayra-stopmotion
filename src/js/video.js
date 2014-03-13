11cb33f9dae1cdcb1e211a12fdc99ee9970aad4d
Alumno <alumno@huayra.(none)>
2014-03-13 10:34:08 -0300

    probando la invocacion directa a uvccapture

--- src/js/video.js
+++ src/js/video.js
@@ -1,60 +1,61 @@
-var onFailSoHard = function(e) {
-  var video = document.querySelector('video');
-  video.src = 'media/error_camara.webm';
-  video.play();
-  console.log("Error al capturar la camara.");
-  
-  
-  var Camelot = require('camelot');
+var numero = 0;
+var prefijo = Math.random();
+var video = document.querySelector('video');
+var exec = require('child_process').exec;
 
-  var camelot = new Camelot( {
-		device : '/dev/video1',
-    'rotate' : '180',
-    'flip' : 'v',
-		controls: {
-			focus: 0
-		}
-  });
+/**
+ * Capturador de video que se utiliza si el equipo
+ * no soporta acceso a la webcam utilizando HTML5.
+ */
+function CapturadorUVC() {
+    var self = this;
 
-  camelot.on('frame', function (image) {
-    console.log('frame received!');
-    
-  	require('fs').writeFile('./pepe.png', image, function (err) {
-      if (err)
-        throw err;
-      console.log('saved!');
-    });
-    
-  });
+    this.cuando_obtiene_captura = function(ruta) {
+        var img = document.getElementsByTagName("img")[3];
 
-  camelot.on('error', function (err) {
-    console.log(err);
-  });
+        if (img) {
+            img.src = ruta;
+        }
 
-  camelot.grab( {
-    'title' : 'Camelot',
-    'font' : 'Arial:24',
-    'frequency' : 1,
-    controls: {
-      focus: 0,
     }
-  });
+
+    this.iniciar = function() {
+        console.log("iniciando");
+
+        function capturar() {
+            console.log("capturando...");
+
+            exec('uvccapture -m', function(error, stdout, stderr) {
+                self.cuando_obtiene_captura('snap.jpg');
+                console.log(error, stdout, stderr);
+                setTimeout(capturar, 1000);
+            });
+        }
+
+        capturar();
+    }
+    
+}
+
+function cuando_falla(error) {
+  console.log(error);
+  //var video = document.querySelector('video');
+  //video.src = 'media/error_camara.webm';
+  //video.play();
   
-  
-};
+  var capturador = new CapturadorUVC(); 
+  capturador.iniciar();
+}
+
+function cuando_obtiene_stream(stream) {
+  video.src = window.URL.createObjectURL(stream);
+}
 
 window.URL = window.URL || window.webkitURL;
-navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-var video = document.querySelector('video');
-  
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
 if (navigator.getUserMedia) {
-  navigator.getUserMedia({audio: false, video: true}, function(stream) {
-    video.src = window.URL.createObjectURL(stream);
-  }, onFailSoHard);
-} else {
-  console.log("No se puede obtener getusermedia.");
-  video.src = 'media/error_camara.webm';
-  video.play();
+  var medios = {audio: false, video: true};
+
+  navigator.getUserMedia(medios, cuando_obtiene_stream, cuando_falla);
 }
