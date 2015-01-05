b2fae11318b6d673e66b603ba40dc36fc6c80bf9
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-01-05 16:50:25 -0300

    Agregando un notificador de versión.

---
+++ src/js/directives/huayra-version.js
@@ -0,0 +1,42 @@
+var app = angular.module('app');
+
+app.directive('huayraVersion', function() {
+  return {
+    restrict: "E",
+    transclude: true,
+    controller: function($scope, $http) {
+      $scope.data = {};
+      $scope.data.version = "0.4.10";
+      $scope.data.changelog_sorted = [];
+      $scope.data.changelog_visible = false;
+
+      $scope.show_changelog = function() {
+        $scope.data.changelog_visible = true;
+      };
+
+      $scope.hide_changelog = function() {
+        $scope.data.changelog_visible = false;
+      };
+
+
+      $http.get('./changelog.json').then(function(result) {
+        $scope.data.changelog = result.data;
+
+        for (var i=0; i<result.data.all_tags.length; i++) {
+          var tag_name = result.data.all_tags[i];
+
+          if (tag_name === 'current')
+            continue;
+
+          var item = result.data.changelog[tag_name];
+          $scope.data.changelog_sorted.unshift(item);
+        }
+
+      });
+    },
+    scope: {
+      url_new_version: "&",
+    },
+    templateUrl: "partials/directives/huayra-version.html",
+  };
+});
