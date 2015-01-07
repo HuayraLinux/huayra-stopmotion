3049323fd00f21e14758943951055d23cd303e03
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-01-07 12:24:56 -0300

    Comenzando a modularizar componentes.

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,1 +1,24 @@
-module.export = {}
+window.onerror = function(e) {
+  alert(e);
+};
+
+window.mostrar_herramientas_de_desarrollo = function() {
+  var w = gui.Window.get();
+  w.showDevTools();
+}
+
+window.mostrar = function(elemento, ruta_a_imagen) {
+  /* Se ejecuta cuando la imagen del timeline está lista para ser mostrada. */
+
+  setTimeout(function() {
+    elemento.classList.remove('img-invisible');
+    elemento.classList.add('img-visible');
+    elemento.src = ruta_a_imagen;
+
+    try {
+      elemento.parentElement.classList.remove('cargando');
+    } catch(e) {
+      console.error(e);
+    }
+  }, 2000);
+}
