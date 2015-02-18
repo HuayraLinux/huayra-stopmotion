846a50bf3c37bf454c697bfd681d14f3751e1d2a
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-02-18 11:40:53 -0300

    Simplificando la directiva huayra-version.

--- src/js/directives/huayra-version.js
+++ src/js/directives/huayra-version.js
@@ -8,31 +8,6 @@ app.directive('huayraVersion', function() {
       $scope.data = {};
       $scope.data.version = "0.4.16";
       $scope.data.changelog_sorted = [];
-      $scope.data.changelog_visible = false;
-
-      $scope.show_changelog = function() {
-        $scope.data.changelog_visible = true;
-      };
-
-      $scope.hide_changelog = function() {
-        $scope.data.changelog_visible = false;
-      };
-
-
-      $http.get('./changelog.json').then(function(result) {
-        $scope.data.changelog = result.data;
-
-        for (var i=0; i<result.data.all_tags.length; i++) {
-          var tag_name = result.data.all_tags[i];
-
-          if (tag_name === 'current')
-            continue;
-
-          var item = result.data.changelog[tag_name];
-          $scope.data.changelog_sorted.push(item);
-        }
-
-      });
     },
     scope: {
       url_new_version: "&",
