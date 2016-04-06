5c3dfb4ed39c3d76d97357ba375948d39aa15b15
Mauro <lavaramano@gmail.com>
2016-04-06 15:09:03 -0300

    typo

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,4 @@ window.VERSION = "0.2";
 
 var app = angular.module('app', ['ngAnimate', 'ui.bootstrap']);
 var fs = require('fs');
-fs.writeFileSynch('/tmp/huayra-stopmotion.pid', process.pid);
+fs.writeFileSync('/tmp/huayra-stopmotion.pid', process.pid);
