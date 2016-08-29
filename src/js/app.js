56f0fa207c3c09661dc1940911def575d68fda35
Ignacio Losiggio <iglosiggio@gmail.com>
2016-08-29 13:19:46 -0300

    Quitado el node-main

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 window.VERSION = "0.2";
 
-/* Hack para setear WM_CLASS */
-process.mainModule.exports.init(require('nwjs-hack').set_wmclass.bind(null, "huayra-motion", true));
-
 var app = angular.module('app', ['ngAnimate', 'ui.bootstrap']);
 var fs = require('fs');
+
+/* Hack para setear WM_CLASS */
+require('nwjs-hack').set_wmclass(null, "huayra-motion", true);
 fs.writeFileSync('/tmp/huayra-stopmotion.pid', process.pid);
