b60a9b1b81ca050315ebeb6ce438393159b4aed3
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-10-17 14:28:35 -0300

    Comenzando a crear el layout inicial.

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'huayra-stopmotion',
     environment: environment,
-    rootURL: '/',
-    locationType: 'auto',
+    rootURL: null,
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -29,6 +29,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
+    
     ENV.locationType = 'none';
 
     // keep test console output quieter
@@ -39,7 +40,6 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
   }
 
   return ENV;
