789a1cbff78e5d072e6f0dbdb4f2d06519196214
Ignacio Losiggio <iglosiggio@gmail.com>
2016-12-01 12:42:41 -0300

    Mantener la grilla al cambiar de resolución de la cámara

--- app/components/huayra-grilla.js
+++ app/components/huayra-grilla.js
@@ -16,37 +16,39 @@ export default Ember.Component.extend({
     dashFormat: [], /* https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/setLineDash */
   },
 
-  grilla: Ember.observer('config.filas', 'config.columnas', 'config.style', 'config.lineWidth', 'config.dashFormat', function() {
-    var width = this.get('width');
-    var height = this.get('height');
-    var filas = this.get('config.filas');
-    var columnas = this.get('config.columnas');
-    var style = this.get('config.style');
-    var lineWidth = this.get('config.lineWidth');
-    var dashFormat = this.get('config.dashFormat');
+  grilla: Ember.observer('config.filas', 'config.columnas', 'config.style', 'config.lineWidth', 'config.dashFormat', 'width', 'height', function() {
+    return new Ember.run.later(() => {
+      var width = this.get('width');
+      var height = this.get('height');
+      var filas = this.get('config.filas');
+      var columnas = this.get('config.columnas');
+      var style = this.get('config.style');
+      var lineWidth = this.get('config.lineWidth');
+      var dashFormat = this.get('config.dashFormat');
 
-    var canvas = this.get('element');
-    var ctx = canvas.getContext('2d');
+      var canvas = this.get('element');
+      var ctx = canvas.getContext('2d');
 
 
-    ctx.clearRect(0, 0, width, height);
-    ctx.setLineDash(dashFormat);
-    ctx.lineWidth = lineWidth;
-    ctx.strokeStyle = style;
+      ctx.clearRect(0, 0, width, height);
+      ctx.setLineDash(dashFormat);
+      ctx.lineWidth = lineWidth;
+      ctx.strokeStyle = style;
 
-    ctx.beginPath();
+      ctx.beginPath();
 
-    for(var x = width / columnas; x < width; x += width / columnas) {
-      ctx.moveTo(x, 0);
-      ctx.lineTo(x, height);
-    }
+      for(var x = width / columnas; x < width; x += width / columnas) {
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, height);
+      }
 
-    for(var y = height / filas; y < height; y += height / filas) {
-      ctx.moveTo(0, y);
-      ctx.lineTo(width, y);
-    }
+      for(var y = height / filas; y < height; y += height / filas) {
+        ctx.moveTo(0, y);
+        ctx.lineTo(width, y);
+      }
 
-    ctx.stroke();
+      ctx.stroke();
+    });
   }),
 
   didInsertElement() {
