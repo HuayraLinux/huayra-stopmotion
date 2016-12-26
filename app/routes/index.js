dd971cd1423e35e894220e9a2171cbddd3c5009d
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-12-26 14:46:09 -0300

    Creando un botón para abrir proyectos y mejorando la ruta de creación de proyectos.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  electron: Ember.inject.service(),
+
+  setupController(controller) {
+    controller.set('inElectron', inElectron);
+  },
+
   actions: {
     abrirPruebaDeCamaras() {
       this.transitionTo('camaras');
@@ -11,5 +17,16 @@ export default Ember.Route.extend({
     crearUnProyectoNuevo() {
       this.transitionTo('editor.new');
     },
+    abrirUnProyectoExistente() {
+      this.get('electron').seleccionarUnProyectoConDialogoDeSistema().then((ubicacion) => {
+        this.transitionTo('editor.editar', encodeURIComponent(ubicacion));
+      }, (error) => {
+        if (error) {
+          alert("ERROR " + error);
+        } else {
+          console.warn("El usuario ha cancelado el dialogo.");
+        }
+      });
+    }
   }
 });
--- app/services/electron.js
+++ app/services/electron.js
@@ -36,6 +36,10 @@ export default Ember.Service.extend(Ember.Evented, {
 
       electron.remote.dialog.showOpenDialog(opciones, (directorio) => {
 
+        if (!directorio) {
+          return reject("No ha seleccionado un directorio.");
+        }
+
         // Verifica que se pueda escribir sobre el directorio que eleligió el usuario.
         fs.access(directorio[0], fs.W_OK, function(err) {
 
@@ -64,6 +68,44 @@ export default Ember.Service.extend(Ember.Evented, {
     } else {
       return false;
     }
+  },
+
+
+  seleccionarUnProyectoConDialogoDeSistema() {
+
+    return new Ember.RSVP.Promise((success, reject) => {
+      let fs = requireNode('fs');
+      let electron = requireNode('electron');
+      let opciones = {properties: ['openDirectory']};
+
+      electron.remote.dialog.showOpenDialog(opciones, (directorio) => {
+
+        if (! directorio) {
+          return reject();
+        }
+
+        let directorioSeleccionado = directorio[0];
+
+        // Verifica que se pueda escribir sobre el directorio que eleligió el usuario.
+        fs.access(directorioSeleccionado, fs.W_OK, (err) => {
+
+          if (err) {
+            reject("Tiene que elegir un directorio que se pueda escribir.");
+          } else {
+
+            if (this.existeProyectoEnLaRuta(directorioSeleccionado)) {
+              success(directorioSeleccionado);
+            } else {
+              reject("No existe ningún proyecto en este directorio.");
+            }
+
+          }
+
+        });
+
+      });
+    });
+
   }
 
 });
