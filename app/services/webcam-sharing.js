c82f70062b18038e384a12d7d67f87021d18fc03
Ignacio Losiggio <iglosiggio@gmail.com>
2016-12-28 15:44:16 -0300

    Reparando los tests del servicio de compartición

--- app/services/webcam-sharing.js
+++ app/services/webcam-sharing.js
@@ -1,52 +1,58 @@
 import Ember from 'ember';
 
-const Promise = Ember.RSVP.Promise;
-const createServer = requireNode('webcam-http-streaming').createHTTPStreamingServer;
+const service = Ember.Service.extend({});
 
-export default Ember.Service.extend({
-  server: null,
-  camaras: Ember.inject.service(),
-  webcams: Ember.computed.alias('camaras.camaras'),
+export default service;
 
-  port: Ember.computed('server', function() {
-    return this.get('server').address().port;
-  }),
+if(inElectron) {
+  const Promise = Ember.RSVP.Promise;
+  const createServer = requireNode('webcam-http-streaming').createHTTPStreamingServer;
 
-  createServer: Ember.on('init', function() {
-    /* x264 over matroska */
-    const encoder = {
-      command: 'avconv',
-      flags: (webcam) => `-f video4linux2 -i ${webcam} -f matroska -vcodec libx264 -acodec none -preset ultrafast pipe:1`,
-      mimeType: 'video/x-matroska'
-    };
+  service.reopen({
+    server: null,
+    camaras: Ember.inject.service(),
+    webcams: Ember.computed.alias('camaras.camaras'),
 
-    const options = {
-      isValidWebcam: (webcam) => new Promise
-        ((accept, refuse) =>
-          this.get('webcams').find((localWebcam) => webcam === localWebcam.DEVNAME)?
-            accept(webcam) : refuse(false)),
-      additionalEndpoints: {
-        '/list': (req, res) => {
-          const webcamList = this.get('webcams')
-            .map((webcam) => ({
-              url: `/webcam?webcam=${webcam.DEVNAME}`,
-              name: webcam.ID_V4L_PRODUCT
-            }));
+    port: Ember.computed('server', function() {
+      return this.get('server').address().port;
+    }),
 
-          res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify(webcamList));
-        }
-      },
-      encoder: encoder
-    };
+    createServer: Ember.on('init', function() {
+      /* x264 over matroska */
+      const encoder = {
+        command: 'avconv',
+        flags: (webcam) => `-f video4linux2 -i ${webcam} -f matroska -vcodec libx264 -acodec none -preset ultrafast pipe:1`,
+        mimeType: 'video/x-matroska'
+      };
 
-    const server = createServer(options).listen();
+      const options = {
+        isValidWebcam: (webcam) => new Promise
+          ((accept, refuse) =>
+            this.get('webcams').find((localWebcam) => webcam === localWebcam.DEVNAME)?
+              accept(webcam) : refuse(false)),
+        additionalEndpoints: {
+          '/list': (req, res) => {
+            const webcamList = this.get('webcams')
+              .map((webcam) => ({
+                url: `/webcam?webcam=${webcam.DEVNAME}`,
+                name: webcam.ID_V4L_PRODUCT
+              }));
 
-    server.on('listening', () => {
-      this.notifyPropertyChange('port');
-      console.log('Server listening:', server.address());
-    });
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(webcamList));
+          }
+        },
+        encoder: encoder
+      };
 
-    this.set('server', server);
-  })
-});
+      const server = createServer(options).listen();
+
+      server.on('listening', () => {
+        this.notifyPropertyChange('port');
+        console.log('Server listening:', server.address());
+      });
+
+      this.set('server', server);
+    })
+  });
+}
