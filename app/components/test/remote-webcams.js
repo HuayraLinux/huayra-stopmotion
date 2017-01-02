05a86b02f473ca3992d9f9f214582c99d0ee8c3f
Ignacio Losiggio <iglosiggio@gmail.com>
2017-01-02 16:48:33 -0300

    Usar hostnames para conectarse a las instancias remotas

--- app/components/test/remote-webcams.js
+++ app/components/test/remote-webcams.js
@@ -4,14 +4,15 @@ import DS from 'ember-data';
 export default Ember.Component.extend({
   url: Ember.computed('server', function() {
     const server = this.get('server');
-    return `http://${server.addresses[0]}:${server.port}`;
+    /*const ipv4 = /([0-9]{1,3}\.){3}[0-9]{1,3}/;*/
+    return `${server.host}:${server.port}`;
   }),
 
   webcams: Ember.computed(function() {
     Ember.run.later(() => this.notifyPropertyChange('webcams'), 10000);
 
     return DS.PromiseArray.create({
-      promise: $.getJSON(`${this.get('url')}/list`)
+      promise: $.getJSON(`http://${this.get('url')}/list`)
     });
   })
 });
