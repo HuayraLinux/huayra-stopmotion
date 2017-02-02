ecd60116cd5c90f9b5ee86ee3d76311d39875b11
Ignacio Losiggio <iglosiggio@gmail.com>
2017-02-02 15:53:08 -0300

    Evitar que la cámara quede frenada al cambiar de ruta

--- app/components/huayra-camara.js
+++ app/components/huayra-camara.js
@@ -4,4 +4,8 @@ export default Ember.Component.extend({
 	camaras: Ember.inject.service(),
 	tagName: 'div',
   classNames: ['canvas-layer'],
+
+	didInsertElement() {
+		this.$('video')[0].play();
+	}
 });
