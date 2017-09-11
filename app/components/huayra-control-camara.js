c039e48251af59d97703875d428233c146161e66
Ignacio Losiggio <iglosiggio@gmail.com>
2017-09-11 16:10:45 -0300

    Quitado el changeset de huayra-control-camara

--- app/components/huayra-control-camara.js
+++ app/components/huayra-control-camara.js
@@ -1,20 +1,10 @@
 import Ember from 'ember';
-import Changeset from 'ember-changeset';
 
 export default Ember.Component.extend({
 	tagName: 'fieldset',
 	classNames: ['huayra-control'],
 	camaras: Ember.inject.service(),
 	controls: Ember.computed.alias('camaras.controls'),
-
-/*
-	controls: Ember.computed('camaras.controls', function() {
-		return this.get('camaras.controls').map((control) => new Changeset(control));
-	}),
-*/
-	save() {
-		return this.get('controls').map((control) => control.save());
-	},
 
 	check(control, checked) {
 		control.set('value', checked ? 1 : 0);
