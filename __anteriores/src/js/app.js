5ab1375011295217152e7be61cba5a7d2aba0891
Ignacio Losiggio <iglosiggio@gmail.com>
2017-09-18 10:33:07 -0300

    Borrar la vieja versión de stopmotion del tree

--- __anteriores/src/js/app.js
+++
@@ -1,8 +0,0 @@
-window.VERSION = "0.2";
-
-var app = angular.module('app', ['ngAnimate', 'ui.bootstrap']);
-var fs = require('fs');
-
-/* Hack para setear WM_CLASS */
-require('nwjs-hack').set_wmclass("huayra-motion", true);
-fs.writeFileSync('/tmp/huayra-stopmotion.pid', process.pid);
